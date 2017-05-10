fd3dbaa136229ee45c20ad86123dc7b5a0ed0321
HENRY HOBHOUSE <henryhobhouse@gmail.com>
2017-05-10 15:34:50 +0100

    All functionalities implemented according to guidelines // test working (11 of them)

--- spec/Thermostat_spec.js
+++ spec/Thermostat_spec.js
@@ -1,3 +1,5 @@
+'use strict';
+
 describe("Thermostat", function() {
   var thermostat;
 
@@ -6,37 +8,72 @@ describe("Thermostat", function() {
   });
 
   it("It initializes at 20 degress", function () {
-    expect(thermostat.temperature).toEqual(20)
+    expect(thermostat.current_temperature).toEqual(20)
   });
 
   it("It increases temperature by 1 from 20 to 21 degrees", function () {
     thermostat.uptemp()
-    expect(thermostat.temperature).toEqual(21)
+    expect(thermostat.current_temperature).toEqual(21)
   });
 
   it("It decreases temperature by 1 from 20 to 19 degrees", function () {
     thermostat.downtemp()
-    expect(thermostat.temperature).toEqual(19)
+    expect(thermostat.current_temperature).toEqual(19)
   });
 
-  it("Will not let temperature go below 10", function () {
-    thermostat.temperature = 10;
-    expect(function(){
-      thermostat.downtemp()}).toThrow("Temp is too low");
+it("Will not let temperature go below 10", function () {
+  thermostat.current_temperature = 10;
+  expect(function(){
+    thermostat.downtemp()}).toThrow("Temp is too low");
   });
 
   it("Will not let temperature go above 25 on powersaver ON", function () {
-    thermostat.temperature = 25;
+    thermostat.current_temperature = 25;
     expect(function(){
       thermostat.uptemp()}).toThrow("Temp is too high");
   });
 
   it("Will not let temperature go above 32 on powersaver OFF", function () {
-    thermostat.temperature = 32;
+    thermostat.current_temperature = 32;
     thermostat.powersaver = false;
     expect(function(){
       thermostat.uptemp()}).toThrow("Temp is too high");
   });
+
+  it("Will reset temperature to 20 from 24", function () {
+    thermostat.current_temperature = 24;
+    thermostat.temp_reset();
+    expect(thermostat.current_temperature).toEqual(20);
+  });
+
+  it("Will state low energy usage when temperature is 17", function () {
+    thermostat.current_temperature = 17;
+    expect(thermostat.energy_usage()).toEqual("low_usage");
+  });
+
+  it("Will state medium energy usage when temperature is 23", function () {
+    thermostat.current_temperature = 23;
+    expect(thermostat.energy_usage()).toEqual("medium_usage");
+  });
+
+  it("Will state high energy usage when temperature is 28", function () {
+    thermostat.current_temperature = 28;
+    expect(thermostat.energy_usage()).toEqual("high_usage");
+  });
+
+  it("Switches the power saver OFF >> Max temp 32 and powersaver will be False", function() {
+    thermostat.saver_switch();
+    expect(thermostat.max).toEqual(thermostat.MAX_SAVER_OFF);
+    expect(thermostat.powersaver).toEqual(false);
+  });
+
+  it("Switches the power saver ON >> Max temp 25 and powersaver will be True", function() {
+    thermostat.powersaver = false
+    thermostat.saver_switch();
+    expect(thermostat.max).toEqual(thermostat.MAX_SAVER_ON);
+    expect(thermostat.powersaver).toEqual(true);
+  });
+
 
 
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,32 +1,52 @@
 function Thermostat() {
-  this.temperature = 20;
-  this.min = 10;
+  this.DEFAULT_TEMP = 20;
+  this.current_temperature = this.DEFAULT_TEMP;
+  this.MIN = 10;
   this.powersaver = true;
-  this.max_saver_on = 25;
-  this.max_saver_off = 32;
+  this.MAX_SAVER_ON = 25;
+  this.MAX_SAVER_OFF = 32;
+  this
 };
 
 Thermostat.prototype.uptemp = function() {
   this.check_temp();
-  this.temperature++;
+  this.current_temperature++;
 };
 
 Thermostat.prototype.downtemp = function() {
   this.check_temp();
-  this.temperature--;
+  this.current_temperature--;
 };
 
 Thermostat.prototype.check_temp = function() {
-  this.saver_switch()
-  if (this.temperature === this.min) throw "Temp is too low";
-  if (this.temperature === this.max) throw "Temp is too high";
+  this._check_max();
+  if (this.current_temperature <= this.MIN) throw "Temp is too low";
+  if (this.current_temperature >= this.max) throw "Temp is too high";
 }
 
 Thermostat.prototype.saver_switch = function() {
-  if (this.powersaver === true) {this.max = this.max_saver_on}
-  else {this.max = this.max_saver_off}
+  if (this.powersaver === true) {
+    this.powersaver = false;
+  } else {
+    this.powersaver = true;
+  }
+  this._check_max();
 }
 
-// //thermostat ... SWITCH =
-// power saver. on --> off
-//                 offf --> on
+Thermostat.prototype._check_max = function() {
+  if (this.powersaver === true) {
+    this.max = this.MAX_SAVER_ON;
+  } else {
+    this.max = this.MAX_SAVER_OFF;
+  }
+}
+
+Thermostat.prototype.temp_reset = function() {
+  this.current_temperature = this.DEFAULT_TEMP;
+}
+
+Thermostat.prototype.energy_usage = function() {
+  if (this.current_temperature < 18) {return "low_usage"};
+  if (this.current_temperature >= 18 && this.current_temperature < 25) {return "medium_usage"};
+  if (this.current_temperature >= 25) {return "high_usage"};
+}
