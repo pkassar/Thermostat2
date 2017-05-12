831746a09be74af0c87a0301ee8bf93cd6b8833b
Paul Kassar <paulkassar@gmail.com>
2017-05-12 13:23:39 +0100

    toggle button and all interface working properly

--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,49 +1,45 @@
 function Thermostat() {
   this.DEFAULT_TEMP = 20;
+  this.MAX_SAVER_ON = 25;
+  this.MAX_SAVER_OFF = 32;
   this.current_temperature = this.DEFAULT_TEMP;
   this.MIN = 10;
   this.powersaver = true;
-  // this.max = MAX_SAVER_ON;
-  this.MAX_SAVER_ON = 25;
-  this.MAX_SAVER_OFF = 32;
+  this.max = this.MAX_SAVER_ON;
   this.check_energy_usage();
-  this._check_max();
 };
 
 Thermostat.prototype.uptemp = function() {
-  this._check_temp();
+  this._check_temp_max();
   this.current_temperature++;
   this.check_energy_usage();
 };
 
 Thermostat.prototype.downtemp = function() {
-  this._check_temp();
+  this._check_temp_min();
   this.current_temperature--;
   this.check_energy_usage();
 };
 
-Thermostat.prototype._check_temp = function() {
-  this._check_max();
+Thermostat.prototype._check_temp_min = function() {
   if (this.current_temperature <= this.MIN) throw "Temp is too low";
+}
+
+Thermostat.prototype._check_temp_max = function() {
   if (this.current_temperature >= this.max) throw "Temp is too high";
 }
 
-Thermostat.prototype.saver_switch = function() {
-  if (this.powersaver === true) {
-    this.powersaver = false;
-  } else {
-    this.powersaver = true;
-  }
-  this._check_max();
-}
+Thermostat.prototype.saver_switch_on = function() {
+  this.powersaver = true
+  this.max = this.MAX_SAVER_ON
+  if (this.current_temperature >= this.max) { this.current_temperature = this.max} ;
+// this.check_energy_usage();
+};
 
-Thermostat.prototype._check_max = function() {
-  if (this.powersaver === true) {
-    this.max = this.MAX_SAVER_ON;
-  } else {
-    this.max = this.MAX_SAVER_OFF;
-  }
-}
+Thermostat.prototype.saver_switch_off = function() {
+  this.powersaver = false
+  this.max = this.MAX_SAVER_OFF
+};
 
 Thermostat.prototype.temp_reset = function() {
   this.current_temperature = this.DEFAULT_TEMP;
@@ -51,7 +47,7 @@ Thermostat.prototype.temp_reset = function() {
 }
 
 Thermostat.prototype.check_energy_usage = function() {
-  if (this.current_temperature < 18) {this.energy_usage = "low_usage"};
-  if (this.current_temperature >= 18 && this.current_temperature < 25) {this.energy_usage = "medium_usage"};
-  if (this.current_temperature >= 25) {this.energy_usage = "high_usage"};
+  if (this.current_temperature < 18) { this.energy_usage = "low_usage" };
+  if (this.current_temperature >= 18 && this.current_temperature < 25) { this.energy_usage = "medium_usage"} ;
+  if (this.current_temperature >= 25) { this.energy_usage = "high_usage" };
 }
--- src/features.js
+++ src/features.js
@@ -1,34 +1,43 @@
 $( document ).ready(function() {
   th1 = new Thermostat();
 
-$('#current_temp').text('The thermostat is now set at ' + th1.current_temperature+ ' °C');
-$('#min_temp').text('the min temperature is ' + th1.MIN + ' °C');
-$('#max_temp').text('the current max temperature is ' + th1.max + ' °C');
+  $('#current_temp').text('The thermostat is now set at ' + th1.current_temperature+ ' °C');
+  $('#min_temp').text('the min temperature is ' + th1.MIN + ' °C');
+  $('#max_temp').text('the current max temperature is ' + th1.max + ' °C');
 
-$('#uptemp').click(function() {
-   th1.uptemp();
-   $('#current_temp').text(th1.current_temperature);
-});
+  $('#uptemp').click(function() {
+    th1.uptemp();
+    $('#current_temp').text(th1.current_temperature);
+  });
 
-$('#downtemp').click(function() {
-   th1.downtemp();
-   $('#current_temp').text(th1.current_temperature);
-});
+  $('#downtemp').click(function() {
+    th1.downtemp();
+    $('#current_temp').text(th1.current_temperature);
+  });
 
-$('#temp_reset').click(function() {
-   th1.temp_reset();
-   $('#current_temp').text(th1.current_temperature);
-});
+  $('#temp_reset').click(function() {
+    th1.temp_reset();
+    $('#current_temp').text(th1.current_temperature);
+  });
 
-$("#power_saver_on").click(function() {
-  th1.saver_switch();
-});
+  $("#power_saver_on").click(function() {
+    th1.saver_switch_on();
+    $('#current_temp').text(th1.current_temperature)
+    $('#max_temp').text('the current max temperature is ' + th1.max + ' °C');
+  });
 
-$.get("http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=8d768cdf677e80b711b9f48b07da13fc&units=metric", function(data) {
-  $("#weather").text("London is " + data.main.temp + '°C    ☺');
-});
+  $("#power_saver_off").click(function() {
+    th1.saver_switch_off();
+    $('#current_temp').text(th1.current_temperature)
+    $('#max_temp').text('the current max temperature is ' + th1.max + ' °C');
+  });
+
+
+  $.get("http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=8d768cdf677e80b711b9f48b07da13fc&units=metric", function(data) {
+    $("#weather").text("London is " + data.main.temp + '°C    ☺');
+  });
 
 
 
-//insert before end
+  //insert before end
 });
